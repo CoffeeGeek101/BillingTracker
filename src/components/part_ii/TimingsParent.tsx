a728b469fa42566b20fc66ce5b943623d7723e4d
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-16 13:18:27 +0530

    Timings Components

---
+++ src/components/part_ii/TimingsParent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimingsParent = () => {
+  return (
+    <div className='flex flex-col basis-4/12'>
+        TimingsParent
+    </div>
+  )
+}
+
+export default TimingsParent
