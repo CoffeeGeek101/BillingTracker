75286971d6def1808f7ba5be8f92173ea54f4c73
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-16 19:10:05 +0530

    Hero Structure implemneted w/ TrackCards

--- src/components/part_i/Hero.tsx
+++ src/components/part_i/Hero.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import TrackingCards from './TrackingCards'
+import { FileCheck, Folder } from 'lucide-react'
 
 const Hero = () => {
   return (
-    <div className='basis-9/12'>
-        Hero
+    <div className='basis-9/12 flex flex-col p-6 text-white'>
+        <div className='flex flex-col gap-8'>
+          <p className='text-4xl'>Activity Tracking.</p>
+          <div className='flex flex-row justify-start gap-5'>
+           <TrackingCards buttonLabel='START' color='pink' icon={<Folder/>} stats='13' title='Task remaining' />
+           <TrackingCards buttonLabel='REVIEW' color='violet' icon={<FileCheck/>} stats='115' title='Completed Task' />
+          </div>
+        </div>
+        <div></div>
+        <div></div>
     </div>
   )
 }
